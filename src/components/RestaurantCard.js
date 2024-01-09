6165da952c2b28cb2aec91c1ee956f1705fd068d
Suhas kachre <suhas.kachre@celoxis.com>
2024-01-09 19:05:12 +0530

    first commit

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,19 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { cloudinaryImageId, name, cuisines, avgRatingString, costForTwo, sla } = resData?.info;
+  return (
+    <div className="res-card">
+      <img className="res-logo" alt="res-logo" src={CDN_URL + cloudinaryImageId} />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRatingString}stars </h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString}</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
